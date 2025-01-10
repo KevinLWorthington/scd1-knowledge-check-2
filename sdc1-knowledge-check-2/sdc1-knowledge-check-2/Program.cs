237bc0ad437bb6f20e24175a2c750299bcb8d562
Kevin Worthington <kevinlworthington@gmail.com>
2025-01-10 17:16:02 -0500

    added additional lines

--- sdc1-knowledge-check-2/sdc1-knowledge-check-2/Program.cs
+++ sdc1-knowledge-check-2/sdc1-knowledge-check-2/Program.cs
@@ -7,46 +7,55 @@ class Program
 {
     static void Main()
     {
-        List<Orders> recordList = new List<Orders>();
+        Console.WriteLine("How many orders do you want to add? ");
+        var numberOfRecords = int.Parse(Console.ReadLine());
 
-        Console.WriteLine("Enter Customer Order Data:");
-        Console.Write("Name: ");
-        string? name = Console.ReadLine();
-        Console.Write("Part Number: ");
-        int part = Convert.ToInt32(Console.ReadLine());
-        Console.Write("Part Quantity: ");
-        int quantity = Convert.ToInt32(Console.ReadLine());
-        Console.Write("Description: ");
-        string? day = Console.ReadLine();
-        Console.Write("Is the order a QCS trial? (Y/N): ");
-        string? trial = Console.ReadLine();
-
-        Orders order = new()
+        for (int i = 0; i < numberOfRecords; i++)
         {
-            Name = name,
-            PartNumber = part,
-            Quantity = quantity,
-            DayofWeek = day,
-            Trial = trial
-        };
+            List<Orders> recordList = new List<Orders>();
 
-        recordList.Add(order);
+            Console.WriteLine("Enter Customer Order Data:");
+            Console.Write("Name: ");
+            string? name = Console.ReadLine();
+            Console.Write("Part Number: ");
+            int part = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Part Quantity: ");
+            int quantity = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Description: ");
+            string? day = Console.ReadLine();
+            Console.Write("Is the order a QCS trial? (Y/N): ");
+            string? trial = Console.ReadLine();
 
-        Console.WriteLine("Record List:");
-        foreach (var item in recordList)
-        {
-            try
+            Orders order = new()
             {
-                Console.WriteLine(item.ToString());
-            }
-            catch (Exception ex)
+                Name = name,
+                PartNumber = part,
+                Quantity = quantity,
+                DayofWeek = day,
+                Trial = trial
+            };
+
+            recordList.Add(order);
+
+
+            Console.WriteLine("Record List:");
+            foreach (var item in recordList)
             {
-                Console.WriteLine("Error printing order: " + ex.Message);
+                try
+                {
+                    Console.WriteLine(item.ToString());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error printing order: " + ex.Message);
+                }
             }
+
+            Console.WriteLine("Number of records: " + recordList.Count);
+
+            Console.WriteLine("Press any key to continue...");
+
+            Console.ReadKey();
         }
-
-        Console.WriteLine("Number of records: " + recordList.Count);
-
-        Console.ReadKey();
     }
 }
